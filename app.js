d5b7bddc92cd5d7412a5fe5ecc1853a7e3317268
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2016-10-13 16:14:06 +0300

    adding and editing attribute for elements

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-angular.module("app", ["dndLists", "ngSanitize"]);
+angular.module("app", ["dndLists", "ngSanitize", "ui.bootstrap"]);
 angular.module("app").controller("SimpleDemoController", function($compile, $scope, $sce) {
+
+    //change default settings in this object
+    $scope.emptyObject = {
+        value: "",
+        required: false,
+        title: "title here",
+        description: "description here"
+    };
+
+
+
     $scope.model = {
         'builder': []
     };
 
-    // $scope.choices = [];
+    $scope.components = {
+        list: []
+    };
 
+    $scope.dynamicPopover = {
+        content: 'Hello, World!',
+        templateUrl: 'components/popoverTemplate.html',
+        test: $scope.components.list
+    };
     $scope.choices = [{
         "label": 'input',
-        "display": '<div class="form-group ng-scope"><label for="" class="col-sm-4 control-label ng-binding" >Text Input</label><div class="col-sm-8"><input type="text" disabled="disabled" ng-model="inputText" validator-required="false" validator-group="" id="" class="form-control ng-pristine ng-valid" placeholder="placeholder"><p class="help-block ng-binding">description</p></div>',
-    }, ];
-    $scope.selected1 = {};
-    $scope.selected2 = {};
+        "display": '<div class="form-group ng-scope"><label for="" class="col-sm-4 control-label ng-binding" >Text Input</label><div class="col-sm-8"><input type="text" disabled="disabled" ng-model="inputText" validator-required="false" validator-group="" id="" class="form-control ng-pristine ng-valid" placeholder="placeholder"><p class="help-block ng-binding">description</p></div></div>',
+    }, {
+        "label": 'submit',
+        "display": '<button class="btn btn-primary">Click</button>',
+    }];
     $scope.submit = function() {
-        console.log($scope.input0);
-        console.log($scope.input1);
+        for (var i = 0; i < $scope.model.builder.length; i++) {
+            console.log($scope.components.list[i]);
+        }
+    };
+
+    $scope.click = function() {
+        window.alert("sayegh");
     };
 
     $scope.createForm = function() {
@@ -29,34 +53,106 @@ angular.module("app").controller("SimpleDemoController", function($compile, $sco
         parent.appendChild(form);
 
         for (var i = 0; i < $scope.model.builder.length; i++) {
-            $scope.render($scope.model.builder[i].label, i);
+            $scope.render($scope.model.builder[i], i);
         }
-        $scope.render("submit", $scope.model.builder.length);
+        $scope.render({
+            label: "submit"
+        }, $scope.model.builder.length);
+        var myOptions = $scope.components.list;
     };
+    oldlength = $scope.model.builder.length;
 
     $scope.$watch('model.builder.length', function(newValue, oldValue) {
+        if (oldlength < newValue){
+          var o2 = JSON.parse(JSON.stringify($scope.emptyObject));
+          $scope.components.list.push(o2);
+        }
         $scope.createForm();
     });
 
-    $scope.render = function(value, index) {
+
+    $scope.makeTextField = function(form, specs, index) {
+        // getting specs specific for this object
+        var myOptions = $scope.components.list[index];
+
+        //initializing dom tags
         var input = document.createElement('input');
+        var ul = document.createElement('ul');
+        var li = document.createElement('li');
+        var divOuter = document.createElement('div');
+        var divInner = document.createElement('div');
+        var label = document.createElement('label');
+        var p = document.createElement('p');
+
+        // configuring outer div
+        ul.setAttribute("class", "simpleDemo");
+
+        //configuring outer div
+        li.setAttribute("class", "simpleDemo");
+        divOuter.setAttribute("uib-popover-template", "dynamicPopover.templateUrl");
+        divOuter.setAttribute("popover-title", "{{components.list["+index+"].title}}");
+        divOuter.setAttribute("popover-placement", "bottom-right");
+        divOuter.setAttribute("ng-click", "setGlobalIndex("+index+")");
+
+        //configuring outer div
+        // uib-popover-template="dynamicPopover.templateUrl" popover-title="{{components.list[$index].title}}" popover-placement="bottom-right"
+        divOuter.setAttribute("class", "form-group myFormItem");
+
+
+        //configuring inner div
+        divInner.setAttribute("class", "col-xs-8");
+
+        //configuring label
+        label.setAttribute("class", "col-xs-4 control-label");
+        label.innerHTML = $scope.components.list[index].title;
+        if (myOptions.required) {
+            label.innerHTML += "*";
+        }
+        $scope.general = {index:0};
+        $scope.setGlobalIndex = function (index) {
+          $scope.general.index = index;
+        };
+        //configuring p
+        p.setAttribute("class", "help-block");
+        p.innerHTML = myOptions.description;
+
+
+        //configuring input
+        input.setAttribute("type", "text");
+        input.setAttribute("placeholder", "placeholder");
+        input.setAttribute("class", "form-control");
+        input.setAttribute("ng-model", "components.list[" + index + "].value");
+        if (myOptions.required) {
+            input.required = true;
+        }
+
+        $compile(input)($scope);
+        $compile(divOuter)($scope);
+        divOuter.appendChild(label);
+        divInner.appendChild(input);
+        divInner.appendChild(p);
+        divOuter.appendChild(divInner);
+        li.appendChild(divOuter);
+        ul.appendChild(li);
+        form.appendChild(divOuter);
+    };
+    $scope.makeSubmitButton = function(form, specs, index) {
+        var input = document.createElement('input');
+        input.setAttribute("type", "submit");
+        input.setAttribute("name", "button");
+        input.setAttribute("class", "btn btn-primary");
+        input.setAttribute("ng-click", "createForm()");
+        $compile(input)($scope);
+        form.appendChild(input);
+    };
+    $scope.render = function(value, index) {
         var form = document.getElementById('form');
-        switch (value) {
+        switch (value.label) {
             case "input":
-                input.setAttribute("type", "text");
-                input.setAttribute("placeholder", "placeholder");
-                input.setAttribute("class", "form-control");
-                input.setAttribute("ng-model", "input" + index);
-                $compile(input)($scope);
-                form.appendChild(input);
+                $scope.makeTextField(form, value, index);
                 break;
             case "submit":
-                input.setAttribute("type", "submit");
-                input.setAttribute("name", "button");
-                input.setAttribute("class", "btn btn-primary");
-                input.setAttribute("ng-click", "createForm()");
-                $compile(input)($scope);
-                form.appendChild(input);
+                $scope.makeSubmitButton(form, value, index);
                 break;
             default:
 
@@ -64,6 +160,10 @@ angular.module("app").controller("SimpleDemoController", function($compile, $sco
         }
 
     };
+
+    $scope.$watch('components.list.length', function(newVal, oldVal) {
+        $scope.createForm();
+    });
 });
 
 
