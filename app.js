aedba5287a091d76a5df31ee36baabdc65178b3f
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2016-10-19 02:18:38 +0300

    added delete option

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 angular.module("app", ["formBuild"]);
 angular.module("app").controller("SimpleDemoController", function($compile, $scope, $sce, builder) {
 
-    // var choice = {
-    //     type: 'radio3',
-    //     display: '<div class=""><div class="form-group"><label for="" class="col-sm-4 control-label">CUSTOM DIRECTIVE</label><div class="col-sm-8"><input type="text" disabled="disabled" class="form-control" placeholder="placeholder"><p class="help-block">description</p></div></div></div>',
-    //     popoverTemplateUrl: 'popover/sample.html',
-    //     htmlTemplate: 'htmlTemplates/sample.html',
-    // };
-    // builder.insert(choice);
-    // $scope.output={};
-
+  
+    $scope.dropCallback = function(event, index, item, external, type, allowedType) {
+        if (external) {
+            if (allowedType === 'itemType' && !item.label) return false;
+            if (allowedType === 'containerType' && !angular.isArray(item)) return false;
+        }
+        $scope.output.splice(index, 1);
+        return item;
+    };
 
 });
 
