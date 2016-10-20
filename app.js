42fbb5fdce37b33798d2a5bda93983c8728f6459
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2016-10-20 03:04:00 +0300

    changed design of gh page

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 angular.module("app", ["formBuild"]);
-angular.module("app").controller("SimpleDemoController", function($compile, $scope, $sce, builder) {
+angular.module("app").controller("ctrl", function($compile, $scope, $sce, builder, $timeout) {
+    $scope.once = true;
+    $scope.scrollTo = function(id) {
+        $('html,body').animate({
+            scrollTop: $('#' + id).offset().top
+        }, 800);
+    };
 
+    $scope.timeup = false;
+    setTimeout(function () {
+      $scope.timeup = true;
+    }, 2000);
+    $scope.$watch("output", function(query) {
+       if ($scope.once && $scope.timeup) {
+           $scope.scrollTo("input");
+           $scope.once = false;
+       }
+    });
 
 });
 
